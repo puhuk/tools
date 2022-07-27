eaa647c36715a693ef85a21f33cc10dda02863d5
puhuk <wetr235@gmail.com>
2022-07-27 22:40:40 +0900

    Update accuracy.py

--- ddp_config/accuracy.py
+++ ddp_config/accuracy.py
@@ -3,9 +3,6 @@ import torch.distributed as dist
 
 
 class Accuracy:
-    def __init__(self):
-        return None
-
     def reset(self):  # reset values with zero
         self._num_correct = 0
         self._num_examples = 0
@@ -13,21 +10,22 @@ class Accuracy:
     def update(
         self, y_pred, y_true
     ):  # update each process's num_correct and num_examples
-        len_ = len(y_true)
-        self._num_correct = (y_true == y_pred).sum()
-        self._num_examples = torch.tensor(len_)
+        self._num_correct += (y_true == y_pred).sum().item()
+        self._num_examples += len(y_true)
 
     def compute(self):
         # collect each processes' num_correct and num_examples
-        self._num_correct = torch.tensor(self._num_correct.numpy())
-        self._num_examples = torch.tensor(self._num_examples.numpy())
-        dist.all_reduce(
-            self._num_correct
-        )  # all processes have same value of num_correct
-        dist.all_reduce(
-            self._num_examples
-        )  # all processes have same value of num_example
+        _num_correct_tmp = torch.tensor(self._num_correct)
+        _num_examples_tmp = torch.tensor(self._num_examples)
 
-        return float(self._num_correct), float(
-            self._num_examples
+        if torch.distributed.is_initialized():
+            dist.all_reduce(
+                _num_correct_tmp
+            )  # all processes have same value of num_correct
+            dist.all_reduce(
+                _num_examples_tmp
+            )  # all processes have same value of num_example
+
+        return float(_num_correct_tmp) / float(
+            _num_examples_tmp
         )  # return num_correct and num_examples separately
