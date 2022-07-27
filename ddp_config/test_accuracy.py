248f72d08852a237c1515d373fc3142f1a828609
puhuk <wetr235@gmail.com>
2022-07-27 15:14:13 +0900

    Update test_accuracy.py

--- ddp_config/test_accuracy.py
+++ ddp_config/test_accuracy.py
@@ -1,5 +1,3 @@
-# python test_accuracy.py
-
 import os
 import pytest
 import torch
@@ -12,21 +10,39 @@ import random
 from accuracy import Accuracy
 import torch.distributed as dist
 
+
 def _test_distrib_accuracy(self):
     rank = dist.get_rank()
     torch.manual_seed(12)
 
-    y_pred_all, y_true_all = (torch.randint(0, 10, size=(40,)), torch.randint(0, 10, size=(40,)))  # 4 x 10 items for all y_pred and all y_true
-    y_pred, y_true = y_pred_all[rank*10:(rank+1)*10], y_true_all[rank*10:(rank+1)*10]  # seperate all y_pred and all y_true to y_pred and y_tre
-    
-    acc = Accuracy()    # initialize the Accuracy class
-    acc.reset()         # reset acc
-    acc.update(y_pred, y_true)  # count the number of correct items and whole items from each process
-    res = acc.compute().cpu().detach().numpy() # compute the accuracy from sum of correct items and whole items of all processes
+    y_pred_all, y_true_all = (
+        torch.randint(0, 10, size=(40,)),
+        torch.randint(0, 10, size=(40,)),
+    )  # 4 x 10 items for all y_pred and all y_true
+    y_pred, y_true = (
+        y_pred_all[rank * 10 : (rank + 1) * 10],
+        y_true_all[rank * 10 : (rank + 1) * 10],
+    )  # seperate all y_pred and all y_true to y_pred and y_tre
 
-    assert accuracy_score(y_pred_all, y_true_all)== pytest.approx(res)  # check with reference value
+    acc = Accuracy()  # initialize the Accuracy class
+    acc.reset()  # reset acc
+    acc.update(
+        y_pred, y_true
+    )  # count the number of correct items and whole items from each process
+    res = (
+        acc.compute()
+    )  # compute the accuracy from sum of correct items and whole items of all processes
+    res2 = acc.compute()
 
-if __name__ == '__main__':
+    print(res, res2)
+
+    assert accuracy_score(y_pred_all, y_true_all) == pytest.approx(
+        res[0] / res[1]
+    )  # check with reference value
+    assert res != res2
+
+
+if __name__ == "__main__":
     backend = "gloo"
     device = idist.device()
     with idist.Parallel(backend=backend, nproc_per_node=4) as parallel:
